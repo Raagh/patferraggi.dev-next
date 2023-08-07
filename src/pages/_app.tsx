c08ae455b72a7f64a5e4b39776f297648838eca7
Patricio Ferraggi Ares <pattferraggi@gmail.com>
2023-08-07 20:39:15 +0200

    add temporary seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import '@/styles/globals.css';
-import {NextPage} from 'next';
-import {AppProps} from 'next/app';
-import {ReactElement, ReactNode} from 'react';
+import { NextPage } from 'next';
+import {NextSeo} from "next-seo";
+import { AppProps } from 'next/app';
+import { ReactElement, ReactNode } from 'react';
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -11,11 +12,41 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 
-const App = ({Component, pageProps}: AppPropsWithLayout) => {
+const meta = {
+  title: `Patricio Ferraggi Ares`,
+  description: `Personal Website and Blog`,
+  url: 'https://www.patferraggi.dev',
+  twitter: `patferraggi`,
+};
+
+const App = ({ Component, pageProps }: AppPropsWithLayout) => {
   const getLayout = Component.getLayout ?? (page => page);
 
   return (
     <>
+      <NextSeo
+        title={meta.title}
+        description={meta.description}
+        canonical={meta.url}
+        openGraph={{
+          url: meta.url,
+          title: meta.title,
+          description: meta.description,
+          images: [
+            {
+              url: "../../../../public/assets/landing/images/2020.jpg",
+              width: 800,
+              height: 420,
+              alt: meta.title,
+            },
+          ],
+        }}
+        twitter={{
+          handle: meta.twitter,
+          site: meta.twitter,
+          cardType: 'summary_large_image',
+        }}
+      />
       {getLayout(<Component {...pageProps} />)}
     </>
   );
