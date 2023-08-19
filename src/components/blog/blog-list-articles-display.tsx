e5fb3e29c906fa3788ffd7f82bd6a82178ccce05
Patricio Ferraggi Ares <pattferraggi@gmail.com>
2023-08-19 12:48:25 +0200

    add articles display to blog page

--- src/components/blog/blog-list-articles-display.tsx
+++ src/components/blog/blog-list-articles-display.tsx
@@ -1,150 +1,46 @@
-import React from "react"
-import { Link } from "gatsby"
-import styled from "styled-components"
-import Img from "gatsby-image"
-import globalStyles from "../../config/style-variables"
-import device from "../../config/device"
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from '@/styles/blog/blogListArticlesDisplay.module.css';
 
-const BlogListArticle = styled.article`
-  width: 25%;
-  min-height: 100%;
-  margin-top: 4rem;
-  display: flex;
-  flex-direction: column;
-
-  @media ${device.small} {
-    width: 100%;
-    margin-top: 2rem;
-  }
-
-  @media ${device.medium} {
-    width: 50%;
-    margin-top: 2rem;
-  }
-
-  @media ${device.large} {
-    width: 33%;
-    margin-top: 2rem;
-  }
-`
-
-const BlogListArticleTitle = styled.h3`
-  margin-top: 1.5rem;
-  width: 70%;
-  min-height: 100px;
-  font-family: ${globalStyles.fontFamilyMedium};
-  font-size: 24px;
-  line-height: 28px;
-  letter-spacing: -1px;
-
-  @media ${device.small} {
-    width: 100%;
-    min-height: 80px;
-  }
-`
-
-const BlogListArticleDate = styled.small`
-  margin-top: auto;
-  min-height: 45px;
-  opacity: 0.5;
-  font-family: ${globalStyles.fontFamilyMedium};
-  font-size: 14px;
-  line-height: 32px;
-`
-
-const StyledGatsbyLink = styled(props => <Link {...props} />)`
-  text-decoration: none;
-  color: ${globalStyles.primaryColor};
-
-  :hover {
-    color: ${globalStyles.secondaryColor};
-  }
-`
-
-const StyledDescription = styled.div`
-  font-size: 18px;
-  line-height: 32px;
-  opacity: 0.8;
-  margin-top: 1rem;
-  font-family: ${globalStyles.fontFamilyMedium};
-
-  @media ${device.small} {
-    line-height: 20px;
-  }
-
-  @media ${device.medium} {
-    line-height: 20px;
-  }
-
-  @media ${device.large} {
-    line-height: 20px;
-  }
-`
-
-const StyledImg = styled(Img)`
-  height: 210px;
-  width: 90%;
-
-  @media ${device.small} {
-    height: 100%;
-    width: 100%;
-    max-height: 130px;
-  }
-
-  @media ${device.medium} {
-    height: 100%;
-    width: 90%;
-    max-height: 130px;
-  }
-
-  @media ${device.large} {
-    height: 100%;
-    width: 90%;
-    max-height: 119px;
-  }
-`
-
-export default props => {
-  const BlogListArticlesListWrapper = styled.section`
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    padding: ${props.sidePadding ? "0 2rem 0 2rem" : 0};
-
-    @media ${device.small} {
-      flex-direction: column;
-    }
-  `
-
-  const posts = props.posts
+export default function BlogListArticlesDisplay(props: any) {
+  const posts = props.posts;
   return (
-    <BlogListArticlesListWrapper>
+    <section className={props.sidePadding ? styles.blogListArticleWrapperPadded : styles.blogListArticleWrapper}>
       {posts.map(({ node }) => {
-        const title = node.frontmatter.title || node.fields.slug
+        const title = node.frontmatter.title || node.fields.slug;
         return (
-          <BlogListArticle key={node.fields.slug}>
-            <StyledGatsbyLink to={`blog${node.fields.slug}`}>
-              <StyledImg
-                imgStyle={{ objectFit: "cover" }}
-                fluid={node.frontmatter.thumbnail.childImageSharp.fluid}
+          <article
+            className={styles.blogListArticle}
+            key={node.fields.slug}
+          >
+            <Link
+              className={styles.styledLink}
+              href={`blog${node.fields.slug}`}
+            >
+              <Image
+                className={styles.styledImage}
+                alt="article"
+                src={node.frontmatter.thumbnail.childImageSharp.fluid.src}
+                width={800}
+                height={800}
               />
-            </StyledGatsbyLink>
+            </Link>
 
-            <BlogListArticleTitle>
-              <StyledGatsbyLink to={`blog${node.fields.slug}`}>
+            <h3 className={styles.blogListArticleTitle}>
+              <Link
+                className={styles.styledLink}
+                href={`blog${node.fields.slug}`}
+              >
                 {title}
-              </StyledGatsbyLink>
-            </BlogListArticleTitle>
-            <StyledDescription>
-              {node.frontmatter.description}
-            </StyledDescription>
-            <BlogListArticleDate>
+              </Link>
+            </h3>
+            <div className={styles.styledDescription}>{node.frontmatter.description}</div>
+            <small className={styles.blogListArticleDate}>
               {node.frontmatter.date} - {node.timeToRead + 1} minutos de lectura
-            </BlogListArticleDate>
-          </BlogListArticle>
-        )
+            </small>
+          </article>
+        );
       })}
-    </BlogListArticlesListWrapper>
-  )
+    </section>
+  );
 }
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,24 +1,138 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Layout from '../layouts/layout';
-import styles from '@/styles/blog/index.module.css'
+import styles from '@/styles/blog/index.module.css';
 import BlogListTemplateHeader from '@/components/blog/blog-list-header';
+import BlogListArticlesDisplay from '@/components/blog/blog-list-articles-display';
 
 export default function Blog() {
-    const { data } = {data: {allMdx: {edges: []}}};
-    const { currentPage, numPages } = { currentPage: 1, numPages: 3};
-    const isFirst = currentPage === 1
-    const isLast = currentPage === numPages
-    const prevPage =
-      currentPage - 1 === 1 ? "/blog/" : "/blog/" + (currentPage - 1).toString()
-    const nextPage = "/blog/" + (currentPage + 1).toString()
+  const { data } = {
+    data: {
+      allMdx: {
+        edges: [
+          {
+            node: {
+              fields: { slug: '/slug' },
+              excerpt:
+                'Hola chicos, ¿cómo han estado? ¿yo? He estado trabajando mucho en un nuevo proyecto que estoy construyendo con la ayuda de un amigo, no…',
+              timeToRead: 4,
+              frontmatter: {
+                title: 'Una semana con Nest.js, ¿está bueno?',
+                description: '',
+                date: '20 marzo 2021',
+                thumbnail: {
+                  childImageSharp: {
+                    fluid: { src: '/assets/landing/blog-article-mock.jpg' },
+                  },
+                },
+              },
+            },
+          },
+          {
+            node: {
+              fields: { slug: '/slug' },
+              excerpt:
+                'Hola chicos, ¿cómo han estado? ¿yo? He estado trabajando mucho en un nuevo proyecto que estoy construyendo con la ayuda de un amigo, no…',
+              timeToRead: 4,
+              frontmatter: {
+                title: 'Una semana con Nest.js, ¿está bueno?',
+                description: '',
+                date: '20 marzo 2021',
+                thumbnail: {
+                  childImageSharp: {
+                    fluid: { src: '/assets/landing/blog-article-mock.jpg' },
+                  },
+                },
+              },
+            },
+          },
+          {
+            node: {
+              fields: { slug: '/slug' },
+              excerpt:
+                'Hola chicos, ¿cómo han estado? ¿yo? He estado trabajando mucho en un nuevo proyecto que estoy construyendo con la ayuda de un amigo, no…',
+              timeToRead: 4,
+              frontmatter: {
+                title: 'Una semana con Nest.js, ¿está bueno?',
+                description: '',
+                date: '20 marzo 2021',
+                thumbnail: {
+                  childImageSharp: {
+                    fluid: { src: '/assets/landing/blog-article-mock.jpg' },
+                  },
+                },
+              },
+            },
+          },
+          {
+            node: {
+              fields: { slug: '/slug' },
+              excerpt:
+                'Hola chicos, ¿cómo han estado? ¿yo? He estado trabajando mucho en un nuevo proyecto que estoy construyendo con la ayuda de un amigo, no…',
+              timeToRead: 4,
+              frontmatter: {
+                title: 'Una semana con Nest.js, ¿está bueno?',
+                description: '',
+                date: '20 marzo 2021',
+                thumbnail: {
+                  childImageSharp: {
+                    fluid: { src: '/assets/landing/blog-article-mock.jpg' },
+                  },
+                },
+              },
+            },
+          },
+          {
+            node: {
+              fields: { slug: '/slug' },
+              excerpt:
+                'Hola chicos, ¿cómo han estado? ¿yo? He estado trabajando mucho en un nuevo proyecto que estoy construyendo con la ayuda de un amigo, no…',
+              timeToRead: 4,
+              frontmatter: {
+                title: 'Una semana con Nest.js, ¿está bueno?',
+                description: '',
+                date: '20 marzo 2021',
+                thumbnail: {
+                  childImageSharp: {
+                    fluid: { src: '/assets/landing/blog-article-mock.jpg' },
+                  },
+                },
+              },
+            },
+          },
+          {
+            node: {
+              fields: { slug: '/slug' },
+              excerpt:
+                'Hola chicos, ¿cómo han estado? ¿yo? He estado trabajando mucho en un nuevo proyecto que estoy construyendo con la ayuda de un amigo, no…',
+              timeToRead: 4,
+              frontmatter: {
+                title: 'Una semana con Nest.js, ¿está bueno?',
+                description: '',
+                date: '20 marzo 2021',
+                thumbnail: {
+                  childImageSharp: {
+                    fluid: { src: '/assets/landing/blog-article-mock.jpg' },
+                  },
+                },
+              },
+            },
+          },
+        ],
+      },
+    },
+  };
+  const { currentPage, numPages } = { currentPage: 1, numPages: 3 };
+  const isFirst = currentPage === 1;
+  const isLast = currentPage === numPages;
+  const prevPage = currentPage - 1 === 1 ? '/blog/' : '/blog/' + (currentPage - 1).toString();
+  const nextPage = '/blog/' + (currentPage + 1).toString();
 
-    const posts = data.allMdx.edges
+  const posts = data.allMdx.edges;
 
-          // <BlogListArticlesDisplay posts={posts}></BlogListArticlesDisplay>
-        // {currentPage === 1 && <SubscribeModal></SubscribeModal>}
-    return (
-      <Layout>
+  // {currentPage === 1 && <SubscribeModal></SubscribeModal>}
+  return (
+    <Layout>
       <Head>
         <title>El Calabozo del Programador</title>
         <meta
@@ -34,31 +148,42 @@ export default function Blog() {
           href="/favicon.ico"
         />
       </Head>
-        <section className={styles.blogListTemplateWrapper}>
-          <BlogListTemplateHeader
-            shouldDisplayMainArticle={currentPage === 1}
-          ></BlogListTemplateHeader>
-          // BlogListArticlesDisplay
-        </section>
-        <ul className={styles.backNextButtons}>
-          {!isFirst && (
-            <button className={styles.styledButton}>
-              <Link className={styles.styledDirectionButton} href={prevPage} rel="prev">
-                ← Anterior
-              </Link>
-            </button>
-          )}
-          {!isLast && (
-            <button className={styles.styledButton}>
-              <Link className={styles.styledDirectionButton} href={nextPage} rel="next">
-                Siguiente →
-              </Link>
-            </button>
-          )}
-        </ul>
-        <Link className={styles.styledNextLink} href="/">← Back to my website</Link>
-        /// SUBSCRIBE MODAL
-      </Layout>
+      <section className={styles.blogListTemplateWrapper}>
+        <BlogListTemplateHeader shouldDisplayMainArticle={currentPage === 1}></BlogListTemplateHeader>
+        <BlogListArticlesDisplay posts={posts}></BlogListArticlesDisplay>
+      </section>
+      <ul className={styles.backNextButtons}>
+        {!isFirst && (
+          <button className={styles.styledButton}>
+            <Link
+              className={styles.styledDirectionButton}
+              href={prevPage}
+              rel="prev"
+            >
+              ← Anterior
+            </Link>
+          </button>
+        )}
+        {!isLast && (
+          <button className={styles.styledButton}>
+            <Link
+              className={styles.styledDirectionButton}
+              href={nextPage}
+              rel="next"
+            >
+              Siguiente →
+            </Link>
+          </button>
+        )}
+      </ul>
+      <Link
+        className={styles.styledNextLink}
+        href="/"
+      >
+        ← Back to my website
+      </Link>
+      /// SUBSCRIBE MODAL
+    </Layout>
   );
 }
 
