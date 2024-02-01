a7840934b5978f7b0509ca83eacf6ad52eec6e19
Patricio Ferraggi Ares <pattferraggi@gmail.com>
2024-02-01 21:26:27 +0100

    add time to read to articles

--- src/lib/blog.ts
+++ src/lib/blog.ts
@@ -1,7 +1,6 @@
 import fs from 'fs';
 import { join } from 'path';
 import matter from 'gray-matter';
-import readingDuration from 'reading-duration' 
 
 const postsDirectory = join(process.cwd(), '_posts');
 
@@ -30,23 +29,49 @@ export function getPostBySlug(slugPath: string) {
   const fullPath = join(postsDirectory, slugPath, 'index.md');
   const fileContents = fs.readFileSync(fullPath, 'utf8');
 
-  const timeToRead = readingDuration(fileContents, { 
-    wordsPerMinute: 100, 
-    emoji: false,
-  })
-  console.log(timeToRead)
+  const wordsPerMinute = 225;
+  const timeToRead = Math.ceil(wordCounter(fileContents) / wordsPerMinute);
+
+  console.log(timeToRead);
 
   const { data, content } = matter(fileContents);
 
-  return {data: { slug: slugPath, timeToRead, ...data } , content};
+  return { data: { slug: slugPath, timeToRead, ...data }, content };
 }
 
 export function getAllPosts() {
   const slugs = getPostSlugs();
-  const posts = slugs
-    .map(slug => getPostBySlug(slug));
-    // sort posts by date in descending order
-    // .sort((post1, post2) => (post1.data.date > post2.data.date ? -1 : 1));
+  const posts = slugs.map(slug => getPostBySlug(slug));
+  // sort posts by date in descending order
+  // .sort((post1, post2) => (post1.data.date > post2.data.date ? -1 : 1));
   //
-  return JSON.parse(JSON.stringify(posts)) 
+  return JSON.parse(JSON.stringify(posts));
+}
+
+function wordCounter(input: string) {
+  const text = input.split(/\s+/);
+  let wordCount = 0;
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] !== ' ' && isWord(text[i])) {
+      wordCount++;
+    }
+  }
+  return wordCount;
+}
+
+function isWord(str: string) {
+  let alphaNumericFound = false;
+  for (let i = 0; i < str.length; i++) {
+    const code = str.charCodeAt(i);
+    if (
+      (code > 47 && code < 58) || // numeric (0-9)
+      (code > 64 && code < 91) || // upper alpha (A-Z)
+      (code > 96 && code < 123)
+    ) {
+      // lower alpha (a-z)
+      alphaNumericFound = true;
+      return alphaNumericFound;
+    }
+  }
+  return alphaNumericFound;
 }
